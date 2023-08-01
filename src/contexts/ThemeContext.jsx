f4498ff6b5f1275ba67ca28066b3f8ca1e3b0599
Prakhar <agrawalprakhar031@gmail.com>
2023-08-02 00:42:50 +0530

    make minor changes in theme context

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,24 +1,26 @@
-import React, {createContext, Component} from 'react'
+import React, { createContext, Component } from "react";
 
 export const ThemeContext = createContext();
 
 class ThemeProvider extends Component {
-    state = { 
-        isLightTheme: true,
-        light: {color: '#010101', ui:'#3BEEB7', bg:'#F4F9FD' },
-        dark: {color:'#ddd', ui: '#333', bg:'#555'}
-     } 
+  state = {
+    isLightTheme: true,
+    light: { color: "#010101", ui: "#3BEEB7", bg: "#F4F9FD" },
+    dark: { color: "#ddd", ui: "#333", bg: "#555" },
+  };
 
-     toggleTheme=()=>{
-        this.setState({isLightTheme: !this.state.isLightTheme})
-     }
-    render() { 
-        return (
-            <ThemeContext.Provider value={{...this.state, toggleTheme:this.toggleTheme}}>
-                {this.props.children}
-            </ThemeContext.Provider>
-        );
-    }
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  };
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
 }
- 
+
 export default ThemeProvider;
